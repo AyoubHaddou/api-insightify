767157196afbbb3b50eb02ebbeb12428478cc87c
Ayoub HADDOU <ahaddou@oversoc.net>
2023-08-01 14:36:19 +0200

    translation add into create_db.py

--- create_db.py
+++ create_db.py
@@ -4,6 +4,7 @@ from app.database.connexion import Base, engine
 from app.database.models.analysis import Analysis 
 from app.database.models.entity import Entity 
 from app.database.models.review import Review
+from app.database.models.translation import Translation
 from app.database.models.tenant import Tenant
 from app.database.seed import seed_db 
 import numpy as np 
